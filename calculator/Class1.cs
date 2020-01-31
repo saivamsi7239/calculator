2beb3eb45c3cff695a4049f9fdd4cbfd9f27bdbb
sai vamsi <saivamsi7799@gmail.com>
2020-01-31 12:48:26 +0530

    Add project files.

---
+++ calculator/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class Class1
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int sub(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+    }
+
+    }
